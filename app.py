8d5baf20f0619e78367adbeefac6ea371631ad8e
Suchitra Behera <suchitrabehera039@gmail.com>
2026-07-08 17:22:53 +0000

    Connect Flask with SQLite database

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
+from config import Config
+from models import db
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'eduaccess_secret_key'
+app.config.from_object(Config)
 
-@app.route('/')
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,1 +1,9 @@
 
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = "eduaccess_secret_key"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "eduaccess.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
